6702cc00f424af531d06959bb6be6a8db690b303
Martin <derminuano@gmail.com>
2019-03-25 11:02:55 -0300

    Login Page

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿namespace Lands.Infrastructure
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Constructor
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+
+    }
+}
